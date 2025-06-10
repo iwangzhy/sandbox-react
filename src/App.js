d591880a20b096873f7ce8c111c7f1e8df7aafa5
wangzhy <iwangzhy@outlook.com>
2025-06-10 18:07:00 +0800

    feat: 传递索引

--- src/App.js
+++ src/App.js
@@ -11,18 +11,37 @@ function Square({ value, onSquareClick }) {
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
-  function handleClick() {
+  function handleClick(i) {
+    // Arrays.prototype.slice() 返回一个新的数组
     const nextSquares = squares.slice();
-    nextSquares[0] = "X";
+    nextSquares[i] = "X";
+    // 闭包
     setSquares(nextSquares);
   }
 
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={handleClick} />
-        <Square />
-        <Square />
+        <Square
+          value={squares[0]}
+          // onXxxx 表示代表事件的 props
+          // handleXxx 表示处理事件的函数
+          onSquareClick={() => {
+            handleClick(0);
+          }}
+        />
+        <Square
+          value={squares[1]}
+          onSquareClick={() => {
+            handleClick(1);
+          }}
+        />
+        <Square
+          value={squares[2]}
+          onSquareClick={() => {
+            handleClick(2);
+          }}
+        />
       </div>
       <div className="board-row">
         <Square />
