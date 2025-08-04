de56e90571ea7cd10ceb9f0d3f809633ba2a232b
wangzhy <iwangzhy@outlook.com>
2025-08-04 17:29:49 +0800

    fix: eslint

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,5 @@
 //  size = 's' 表示 size 的参数默认值是 s
-export function getImageUrl(person: any, size = 's'): string {
+export function getImageUrl(person: { imageId: string }, size = 's'): string {
     return (
         'https://i.imgur.com/' +
         person.imageId +
